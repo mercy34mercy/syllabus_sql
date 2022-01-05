5bb322b4214f1e9bb8a0700b4c55ed7ac7bac2d2
mercy34mercy <etukobamasatyan@gmail.com>
2022-01-05 23:22:33 +0900

    from laptop

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+DATABASE_URL = "postgres://hdskofuyufyqgx:18d75e06e7fd23852717b2f3903245d1b0691c56d8d5746fa66258e167f0019b@ec2-3-209-234-80.compute-1.amazonaws.com:5432/de5lc85kl2p0o"
+
 @app.route('/',methods=['POST','GET'])
 def index():
     return "hello world"
